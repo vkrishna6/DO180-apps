20ab556b4e8d3c32c920ae87d83baa374e00d81e
Student User <student@workstation.lab.example.com>
2023-07-22 12:02:02 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Adding this line after building an application OpenShift Cluster with newbrach. This change should appear in OpenShift. \n";
+print "A change is in the air!\n";
 ?>
